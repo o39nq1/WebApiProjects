9c25deeeb7c2344e55e48adf2d6aba03e55cc8d8
o39nq1 <andras.hortobagyi@stud.uni-corvinus.hu>
2023-11-21 11:03:59 +0100

    vált

--- WebAPIBase/wwwroot/scripts/jsgyak.js
+++ WebAPIBase/wwwroot/scripts/jsgyak.js
@@ -1,22 +1,35 @@
 ﻿window.onload = () => {
     console.log("betöltődött")
-    for (var sor = 0; sor < 10; sor++) {
-        var sor = document.createElement("div")
-        document.getElementById("pascal").appendChild(sor);
+    for (var sor = 0; sor < 10; sor++) { 
         //új div létrehozása az új sornak
+        var ujDiv = document.createElement("div");
         //új div osztálylistájához add hozzá a "sor"-t
+        ujDiv.classList.add('sor');
         //új div-et add hozzá a "pascal" gyermekeihez
+        var pascalDiv = document.getElementById('pascal');
+        pascalDiv.appendChild(ujDiv)
         for (var oszlop = 0; oszlop <= sor; oszlop++) {
-            var oszlop = document.createElement("div");
-            oszlop.innerHTML = '${sor}:${oszlop}';
-            sor.appendChild(oszlop);
             //új div létrehozása az új elemnek
+            var ujElemDiv = document.createElement("div");
             //új elem div osztálylistájához add hozzá az "elem"-et
+            ujElemDiv.classList.add('sor');
             //teszteléshet .innerHTML = `${sor}:${oszlop}`
+            ujElemDiv.innerText = `${sor}:${oszlop}`;
             //legyen az innerHTML a megfelelő szám
+
             //új elem div-et vedd fel a sor elemei közé
+            ujDiv.appendChild(ujElemDiv); 
         }
     }
-}
+
+    var faktoriális = function (n) {
+        let er = 1;
+        for (let i = 2; i <= n; i++) {
+            er = er * i;
+        }
+        return er;
+    }
+
+};
 
 
